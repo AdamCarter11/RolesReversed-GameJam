e493c2b8c70810780fc5b63b16c24a6eab45efe3
Sawyer <sporte@ucsc.edu>
2023-07-08 11:16:44 -0700

    game over scene and tire

---
+++ RolesReversed/Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    [SerializeField] Text highscoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        highscoreText.text =  "Highscore: " + PlayerPrefs.GetInt("HighScore");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.R)) 
+        {
+            //Change Scene Here
+        }
+    }
+}
